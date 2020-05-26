8da3e1892873dd6f76128db8de2f9dd9bef8fdda
Cameron Gagnon <cameron.gagnon@gmail.com>
2020-05-26 02:58:34 -0700

    Fix Oauth token issue; organize clips into folders
    Oauth started to be required for the endpoint I was using to get the
    mp4 information of a clip. Thankfully enough information is returned in
    a clip's metadata to correctly determine the mp4 link.
    
    Clips are also now organized into folders based on the week and then
    game they were clipped in.

--- dl.py
+++ dl.py
@@ -8,6 +8,8 @@ import sys
 import twitch
 import urllib.request
 import yaml
+
+from datetime import timedelta
 from time import sleep
 
 config = None
@@ -19,32 +21,23 @@ with open('config.yml') as stream:
 
 
 client_id = config['client_id']
-client = twitch.TwitchClient(client_id=client_id)
+client = twitch.TwitchClient(config['client_id'])
 clips = client.clips
 
+
 channel = 'stroopc'
-basepath = '/mnt/e/src/adobe/premiere/twitch/twitch_clips/'
+basepath = '/mnt/e/src/adobe/premiere/twitch/twitch_clips'
+# hash of vod_id's to their creation date. Saves looking up the date of the same
+# vod several times if there are several clips from the same vod
+vod_infos = {}
 
 def dl_progress(count, block_size, total_size):
     percent = int(count * block_size * 100 / total_size)
     sys.stdout.write("\r...%d%%" % percent)
     sys.stdout.flush()
 
-def retrieve_mp4_data(slug):
-    clip_info = requests.get(
-        "https://api.twitch.tv/helix/clips?id=" + slug,
-        headers={"Client-ID": client_id}).json()
-
-    if clip_info.get('status', '') == 429:
-        print(clip_info)
-        print("Sleeping 30 seconds...")
-        sleep(30)
-        return retrieve_mp4_data(slug)
-
-    if len(clip_info['data']) == 0:
-        print("Couldn't download clip: ", clip_info)
-        return
-    thumb_url = clip_info['data'][0]['thumbnail_url']
+def format_mp4_data(clip):
+    thumb_url = clip['thumbnails']['medium']
     slice_point = thumb_url.index("-preview-")
     mp4_url = thumb_url[:slice_point] + '.mp4'
     return mp4_url
@@ -68,63 +61,79 @@ def download_clips(channel_clips):
     print("Downloading: {} clips".format(len(channel_clips)))
 
     for clip in channel_clips:
-        out_path, out_f = generate_filename(clip)
-        if not already_downloaded(out_path):
-            download(clip, out_path, out_f)
+        full_output_name = generate_filename(clip)
+        if not already_downloaded(full_output_name):
+            download(clip, full_output_name)
             continue
 
-        out_path_dup, out_filename_dup = generate_filename(clip, iterate=True)
+        full_output_name_dup = generate_filename(clip, iterate=True)
         # needs to be downloaded before we can get its MD5 hash
-        download(clip, out_path_dup, out_filename_dup)
+        download(clip, full_output_name_dup)
         filenames = [file_ for file_ in iterate_filenames(clip)]
         if filenames:
             filenames = filenames[:-1]
         for file_path in filenames:
-            if is_duplicate(file_path, out_path_dup):
-                print("Removing clip: ", out_path_dup, "already downloaded it")
-                os.remove(out_path_dup)
+            if is_duplicate(file_path, full_output_name_dup):
+                print("Already downloaded: ", full_output_name_dup, "...removing")
+                os.remove(full_output_name_dup)
                 break
-            else:
-                print("SAME FILES:", file_path, out_path_dup)
     print("Downloaded {} clips".format(len(channel_clips)))
 
 
-def out_filename(clip):
+def out_filename(clip, i):
     clip_title = clip.title.replace(' ', '_')
     regex = re.compile('[^a-zA-Z0-9_]')
     out_f = regex.sub('', clip_title)
-    return out_f
+    return "{}_{}.mp4".format(out_f, i)
+
+def full_name_for_clip(clip, i):
+    return base_path_for_clip(clip) + out_filename(clip, i)
+
+def base_path_for_clip(clip):
+    clip_created_at = vod_created_date(clip)
+    game = clip['game']
+    # assign the date of the beginning of the week the clip was created. this
+    # puts all clips created in the same week under the same folder.
+    clip_binned_date = clip_created_at - timedelta(days=(clip_created_at.weekday()+1) % 7)
+
+    full_path = "{}/{}/{}".format(basepath,
+            clip_binned_date.strftime("%m-%d-%Y"), game.lower())
+
+    os.makedirs(full_path, exist_ok=True)
+
+    return full_path + '/'
+
+def vod_created_date(clip):
+    global vod_infos
+    vod_id = clip['vod']['id']
+    if not vod_infos.get(vod_id):
+        vod_created_at = client.videos.get_by_id(vod_id)['created_at']
+        vod_infos[vod_id] = vod_created_at
+    return vod_infos[vod_id]
 
 # generates [XXX_0.mp4, ..., XXX_4.mp4] from the clip
 def iterate_filenames(clip):
-    out_f = out_filename(clip)
     i = 0
-    out_file_to_test = basepath + out_f + '_{}.mp4'.format(i)
-    print('out_file_to_test', out_file_to_test)
+    out_file_to_test = full_name_for_clip(clip, i)
     while already_downloaded(out_file_to_test):
         yield out_file_to_test
         i += 1
-        out_file_to_test = basepath + out_f + '_{}.mp4'.format(i)
+        out_file_to_test = full_name_for_clip(clip, i)
 
 def generate_filename(clip, iterate=False):
-    out_f = out_filename(clip)
     i = 0
     if iterate:
-        while already_downloaded(basepath + out_f + '_{}.mp4'.format(i)):
+        while already_downloaded(full_name_for_clip(clip, i)):
             i += 1
 
-    out_f += '_{}.mp4'.format(i)
+    return full_name_for_clip(clip, i)
 
-    out_path = (basepath + out_f)
-    return out_path, out_f
-
-def download(clip, out_path, out_f):
-    print("Downloading", out_f, "from:", clip.url)
+def download(clip, full_output_name):
+    print("Downloading", full_output_name)
     slug = clip.slug
-    mp4_url = retrieve_mp4_data(slug)
+    mp4_url = format_mp4_data(clip)
 
-    print(out_f, mp4_url)
-    urllib.request.urlretrieve(mp4_url, out_path, reporthook=dl_progress)
+    urllib.request.urlretrieve(mp4_url, full_output_name, reporthook=dl_progress)
     print('\nDone.')
 
 def extract_slug(link):
@@ -158,7 +167,7 @@ def main():
             print("Error, please include twitch link or duration in {}".format(durations))
 
     download_clips(channel_clips)
-    print('Finished downloading all the videos.')
+    print('Finished downloading all the clips.')
 
 
 if __name__ == "__main__":
